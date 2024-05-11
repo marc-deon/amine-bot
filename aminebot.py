f105dfd01745b91d14dc951843f26da81c5d30ab
Marc d'Eon <highlyderivative.games@gmail.com>
2024-05-11 09:43:33 -0400

    General improvements

--- aminebot.py
+++ aminebot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Version 2024-04-09
+# Version 2024-05-06
 # Warning: there IS some strangely written cruft in this file that remains as I didn't know better
 # when I wrote those chunks ~4 years ago for another project.
 
@@ -49,15 +49,24 @@ def check_for_updates() -> list:
 def ReadConfig():
     global config
     global shows
-    config = json.load(open(os.path.join(CONFIG_ROOT, "config.json")))
-    shows = json.load(open(os.path.join(CONFIG_ROOT, "shows.json")))
+    try:
+        config = json.load(open(os.path.join(CONFIG_ROOT, "config.json")))
+    except:
+        config = {}
+    try:
+        shows = json.load(open(os.path.join(CONFIG_ROOT, "shows.json")))
+    except:
+        shows = {}
 
     if "MAL_CLIENT_ID" not in config:
         exit("Must have MAL_CLIENT_ID in config!")
     if "MAL_CLIENT_SECRET" not in config:
         exit("Must have MAL_CLIENT_SECRET in config!")
-    token = json.load(open(os.path.join(CONFIG_ROOT, TOKEN_NAME)))
-    config["token"] = token
+    try:
+        token = json.load(open(os.path.join(CONFIG_ROOT, TOKEN_NAME)))
+    except:
+        # ADD HERE
+        pass
 
     for show in shows:
         if "previous_date" in shows[show]:
@@ -67,6 +76,9 @@ def ReadConfig():
 
         if "skipped" not in shows[show]:
             shows[show]["skipped"] = 0
+
+        if "previous_episode" not in shows[show]:
+            shows[show]["previous_episode"] = 0
 
     return config
 
@@ -82,21 +94,26 @@ def get_new_code_verifier():
 
 
 def print_new_authorisation_url(code_challenge:str):
-    global MAL_CLIENT_ID, MAL_CLIENT_SECRET
-
-    url = f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&MAL_client_id={MAL_CLIENT_ID}&code_challenge={code_challenge}'
+    #global MAL_CLIENT_ID, MAL_CLIENT_SECRET
+    MAL_CLIENT_ID = config["MAL_CLIENT_ID"]
+    MAL_CLIENT_SECRET = config["MAL_CLIENT_SECRET"]
+    print("new auth irl")
+    url = f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={MAL_CLIENT_ID}&code_challenge={code_challenge}'
     print(f'Authorise your application by clicking here: {url}\n')
+
 
 # 3. Once you've authorised your application, you will be redirected to the webpage you've
 #    specified in the API panel. The URL will contain a parameter named "code" (the Authorisation
 #    Code). You need to feed that code to the application.
 def generate_new_token(authorisation_code: str, code_verifier: str) -> dict:
     global MAL_CLIENT_ID, MAL_CLIENT_SECRET
+    global token
+    print("new token")
 
     url = 'https://myanimelist.net/v1/oauth2/token'
     data = {
-        'MAL_client_id': MAL_CLIENT_ID,
-        'MAL_client_secret': MAL_CLIENT_SECRET,
+        'client_id': config["MAL_CLIENT_ID"],
+        'client_secret': config["MAL_CLIENT_SECRET"],
         'code': authorisation_code,
         'code_verifier': code_verifier,
         'grant_type': 'authorization_code'
@@ -118,6 +135,7 @@ def generate_new_token(authorisation_code: str, code_verifier: str) -> dict:
 
 def refresh_token(refresh_token:str) -> dict:
     global token
+    print("refresh token")
     url = "https://myanimelist.net/v1/oauth2/token"
 
     data = {
@@ -126,6 +144,7 @@ def refresh_token(refresh_token:str) -> dict:
             "grant_type": "refresh_token",
             "refresh_token": refresh_token
     }
+    print(data)
 
     response = requests.post(url, data=data)
     response.raise_for_status()
@@ -139,13 +158,23 @@ def refresh_token(refresh_token:str) -> dict:
         print('Token saved in "token.json"')
 
     token["aquired"] = now()
+    config["token"] = token
     return token
 
 
 def retrieve_token():
-    with open('/'.join([CONFIG_ROOT, TOKEN_NAME]), 'r') as file:
-        return json.loads(file.read())
-
+    global token
+    try:
+        with open('/'.join([CONFIG_ROOT, TOKEN_NAME]), 'r') as file:
+            return json.loads(file.read())
+    except:
+        code_challenge = get_new_code_verifier()
+        print_new_authorisation_url(code_challenge)
+        authorisation_code = input("Copy-paste the authorisation code: ").strip()
+        token = generate_new_token(authorisation_code, code_challenge)
+        token['aquired'] = now()
+        save_token()
+        return retrieve_token()
 
 def save_token():
     temp = token["aquired"]
@@ -157,8 +186,57 @@ def save_token():
 
 #endregion
 
+def ShowCalendar():
+    import subprocess
+    cal = subprocess.Popen('cal', shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+    weeks = cal.split("\n")
+
+            # Su Mo Tu We Th Fr Sa
+    counts = [0, 0, 0, 0, 0, 0, 0]
+
+    for id, show in shows.items():
+        show["start_date"] = SerializableDatetime.fromisoformat(show["start_date"])
+        dow = int(show["start_date"].strftime('%w'))
+        counts[dow]+=1
+
+    print(counts)
+
+    white = "\033[0m"
+    green = "\033[1;32m"
+    blue  = "\033[1;34m"
+    purple= "\033[35m"
+    orange= "\033[1;33m"
+    end = "\033[0m"
+
+
+    print(weeks[0])
+    print(weeks[1])
+    for week in weeks[2:]:
+        if not week.strip():
+            break
+        days = [
+            week[0:0+2] + end,
+            week[3:3+2] + end,
+            week[6:6+2] + end,
+            week[9:9+2] + end,
+            week[12:12+2] + end,
+            week[15:15+2] + end,
+            week[18:18+2] + end,
+        ]
+        for dow in range(7):
+            n = counts[dow]
+            color = orange if n >= 4 else purple if n >= 3 else blue if n >= 2 else green if n >= 1 else white
+            print(color, days[dow], " ", sep="", end="")
+        print()
+
 if __name__ == "__main__":
     ReadConfig()
+    token = retrieve_token()
+    config["token"] = token
+
+    if "--show_cal" in sys.argv:
+        ShowCalendar()
+        exit()
 
     if "aquired" in config["token"]:
         # stored as string "datetime.datetime(y,m,d,h,m)"
@@ -174,23 +252,28 @@ if __name__ == "__main__":
         config["token"]["aquired"] = SerializableDatetime(1970,1,1)
 
 
-    if (now() - config["token"]["aquired"]).days >= 30:
+    if (now() - config["token"]["aquired"]).days >= 15:
         config["token"] = refresh_token(config["token"]["refresh_token"])
         save_token()
 
     messages = []
-    updated = check_for_updates()
-    for id in updated:
-        # Get the previous date and nickname
-        show = shows[id]
-        # Get rest of info
-        info = mal.get_anime_info(config["token"]["access_token"], id)
-        name = show["nickname"] if "nickname" in show else info["title"]
+    for id, show in shows.items():
 
-        # convert start date + broadcast time to datetime
-        start_date = SerializableDatetime.fromisoformat(info["start_date"])
-        hh, mm = info["broadcast"]["start_time"].split(":")
-        start_date = start_date.replace(hour=int(hh), minute=int(mm), tzinfo=JAPAN)
+        # Get the previous date and nickname
+        # Take from MAL if necessary
+        if "start_date" not in show or "name" not in show:
+            # Get rest of info
+            info = mal.get_anime_info(config["token"]["access_token"], id)
+            show["title"] = info["title"]
+
+            # convert start date + broadcast time to datetime
+            start_date = SerializableDatetime.fromisoformat(info["start_date"])
+            hh, mm = info["broadcast"]["start_time"].split(":")
+            start_date = start_date.replace(hour=int(hh), minute=int(mm), tzinfo=JAPAN)
+            show["start_date"] = start_date
+
+        name = show["nickname"] if "nickname" in show else show["title"]
+        start_date = show["start_date"]
 
         # Get current time
         current_date = now(JAPAN)
@@ -201,7 +284,7 @@ if __name__ == "__main__":
         floating = diff.total_seconds() / SECONDS_IN_WEEK
         episode_num = ceil(floating) - show["skipped"]
 
-        print(name, floating, episode_num, start_date, current_date, "\n", sep="\n")
+        #print(name, floating, episode_num, start_date, current_date, "\n", sep="\n")
 
         # Skip unaired shows
         if episode_num < 1:
@@ -213,22 +296,26 @@ if __name__ == "__main__":
         if info["num_episodes"] > 0 and episode_num > info["num_episodes"]:
             # Create a message for ended shows
             m.message = f"{name} has ended after {info['num_episodes']} episodes."
+            messages.append(m)
 
-        else:
+        elif episode_num > show["previous_episode"]:
             # Create message for new episode
             print("New ep!")
             m.message = f"""{name} episode #{episode_num} is out!"""
             m.link += "\n<https://nyaa.si/?f=0&c=0_0&q=" + urllib.parse.quote(info["title"]) + ">"
-            increment_previous_datetime(id)
+            #increment_previous_datetime(id)
+            show["previous_episode"] = episode_num
+            messages.append(m)
 
-        messages.append(m)
 
-    if "--test" in sys.argv:
-        exit()
-    elif updated:
+    if messages:
         # Save back to file
         SaveShows()
+        print("Saving...")
     else:
+        exit()
+
+    if "--test" in sys.argv:
         exit()
 
     import discord_side
